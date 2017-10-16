7ffd7cd317e4ee1aee12d1668a4d04928706c7e8
Irfan Fediyanto <fandystbiz@gmail.com>
2017-10-16 08:59:58 +0700

    update pertemuan tanggal 9 okt 2017

---
+++ 5p11a/Latihan1/src/HitungLuas.java
@@ -0,0 +1,43 @@
+import javax.microedition.lcdui.*;
+import java.lang.NumberFormatException;
+
+public class HitungLuas extends Form implements CommandListener {
+	TextField txtPanjang;
+	TextField txtLebar;
+	TextField txtHasil;
+	Command btnHitung;
+
+	public HitungLuas() {
+		super("Hitung Luas");
+		txtPanjang = new TextField("Panjang","",10,TextField.NUMERIC);
+		append(txtPanjang);
+
+		txtLebar = new TextField("Lebar    ","",10,TextField.NUMERIC);
+		append(txtLebar);
+
+		txtHasil = new TextField("Hasil      ","",10,TextField.NUMERIC);
+		append(txtHasil);
+
+		btnHitung = new Command("Hitung",Command.OK,0);
+		addCommand(btnHitung);
+
+		setCommandListener(this);
+
+	}
+
+	public void commandAction(Command cmd, Displayable d) {
+
+		if(cmd == btnHitung) {
+			try {
+				int p = Integer.parseInt(txtPanjang.getString());
+				int l = Integer.parseInt(txtLebar.getString());
+				int hasil = p * l;
+
+				txtHasil.setString(hasil+"");
+			}catch(NumberFormatException e) {
+
+			}
+		}
+
+	}
+}
